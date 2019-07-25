019bb332a8f583fec3ec1759e8c9cffaaa8c59b5
BlueCodeThree <carliemhamilton@gmail.com>
2019-07-25 17:35:29 +1000

    added price to items table, added price to seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_24_040635) do
+ActiveRecord::Schema.define(version: 2019_07_25_072722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2019_07_24_040635) do
     t.boolean "sold", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "price", precision: 8, scale: 2
     t.index ["seller_id"], name: "index_items_on_seller_id"
   end
 
