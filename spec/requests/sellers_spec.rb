1b61e992bea94ac775c5ef4734541686ce19de01
BlueCodeThree <carliemhamilton@gmail.com>
2019-08-01 22:53:35 +1000

    got more tests to pass

--- spec/requests/sellers_spec.rb
+++ spec/requests/sellers_spec.rb
@@ -3,14 +3,11 @@ require 'rails_helper'
 RSpec.describe "Seller Request", type: :request do
   describe "GET /seller/:seller_id requests" do
       before(:each) do
-    user = User.create!(email: "aa1@example.com", password: "123testing1", password_confirmation: "123testing1", username: "alice")
-    @item = assign(:item, Item.create!(seller_id: user.id))
-    sign_in(user)
+      @user = User.create!(email: "aa1@example.com", password: "123testing1", password_confirmation: "123testing1", username: "alice")
   end
 
     it "gets seller_path" do
-
-      get seller_path
+      get seller_path(@user.id)
       expect(response).to have_http_status(200)
     end
   end
