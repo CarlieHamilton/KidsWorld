c7ddac6f16cafcc8082e4531812801e1fb2d379f
BlueCodeThree <carliemhamilton@gmail.com>
2019-08-05 13:42:25 +1000

    added purchases.destroy_all to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Purchase.destroy_all
 Item.destroy_all
 User.destroy_all
 
